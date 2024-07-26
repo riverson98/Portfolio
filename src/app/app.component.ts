0373763e8c362eebb9c968362980ecb25db0e358
riverson98 <riversonvicente@gmail.com>
2024-07-26 12:39:56 -0300

    Init 🎉 - Start of portfolio-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Home',
+        loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent),
+        
+    },
+    {
+        path: 'about',
+        title: 'About',
+        loadComponent: () => import('./components/about/aboutme.component').then(m => m.AboutmeComponent)
+    },
+    {
+        path: 'skills',
+        title: 'Skills',
+        loadComponent: () => import('./components/skills/skills.component').then(m => m.SkillsComponent)
+    }
+];
